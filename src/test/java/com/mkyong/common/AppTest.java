d7ab71e92d21a82d5f2e370f0b16abbb22a65c23
amurgai <ashish_murgai@optum.com>
2017-08-30 18:15:07 +0530

    First Git Code

---
+++ src/test/java/com/mkyong/common/AppTest.java
@@ -0,0 +1,41 @@
+package com.mkyong.common;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+    	super( testName );
+        System.out.println("Inside test suit");
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+    	System.out.println("Inside test suit");
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+    	System.out.println("Inside test App Test Case");
+        assertTrue( true );
+    }
+}
